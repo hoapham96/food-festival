ca1d040ed1c6ac756c2c76aa6d2c1415a5541913
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-03-16 14:07:17 -0500

    Analyze Browser Resource Allocation

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
 
@@ -15,6 +16,9 @@ module.exports = {
         $: "jquery",
         jQuery: "jquery"
       }),
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      })
     ],
     mode: 'development'
 
